1b65f507e3bd0fc5651b683b767f2059e2f6d531
Ryan Wedoff <senor.rw@gmail.com>
2016-03-10 01:51:42 -0600

    Random fixes

--- app/src/main/java/com/ryanwedoff/senor/naoservercontroller/HelpScreen.java
+++ app/src/main/java/com/ryanwedoff/senor/naoservercontroller/HelpScreen.java
@@ -2,6 +2,7 @@ package com.ryanwedoff.senor.naoservercontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HelpScreen extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HelpScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_screen);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
